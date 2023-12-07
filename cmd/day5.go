e3552e0e8abcbffda377ad2435111f395925dffc
Darian Morrison <darianjmorrison@gmail.com>
2023-12-06 17:17:28 -0800

    Parse initial input and some foundation.

--- cmd/day5.go
+++ cmd/day5.go
@@ -5,7 +5,57 @@ import (
 	"fmt"
 	"aoc"
 	"bufio"
+	"strings"
+	"strconv"
+	"log"
 )
+
+type AlmanacMap struct{
+	srcStart int
+	srcEnd int
+	destStart int
+}
+
+type AlmanacMapList struct{
+	destination string
+	maps []AlmanacMap
+}
+
+func (mapList *AlmanacMapList) FindDestination (input int) int{
+	for _, almanacMap := range mapList.maps {
+		if input <= almanacMap.srcEnd && input >= almanacMap.srcStart {
+			return almanacMap.destStart + input - almanacMap.srcStart
+		}
+	}
+
+	return input
+}
+
+func BuildMap(filename string) map[string]AlmanacMapList {
+	return nil			
+}
+
+func ParseInput(filename string) ([]int,map[string]AlmanacMapList) {
+	file := aoc.MustOpenFile(filename)
+	defer file.Close()
+	
+	scanner := bufio.NewScanner(file)
+	var seedNumbers []int
+
+	if scanner.Scan() {
+		seedStrings := strings.Fields(strings.Split(scanner.Text(), ": ")[1])
+		
+		for _, str := range seedStrings {
+			seed, err := strconv.Atoi(str)
+			if err != nil{
+				log.Fatal("Error converting seed (%s) to int: %v\n", str, err)
+			}
+			seedNumbers = append(seedNumbers, seed)
+		}
+		return seedNumbers, nil
+	}	
+	return nil, nil	
+}	
 
 func main(){
 	args := os.Args[1:]
@@ -17,17 +67,9 @@ func main(){
 		inputPath = args[0]
 		fmt.Printf("Using %s as input\n", inputPath)
 	}
-	file := aoc.MustOpenFile(inputPath)
-	defer file.Close()
-	
-	scanner := bufio.NewScanner(file)
-
-	count := 0	
-	for scanner.Scan(){
-		scanner.Text()	
-		count++
-	}		
-
-	fmt.Printf("Number of lines: %d\n", count)
+	seedNumbers, _ := ParseInput(inputPath)
+	for _, seed := range seedNumbers {
+		fmt.Printf("Seed: %d\n", seed)
+	}
 }
 
